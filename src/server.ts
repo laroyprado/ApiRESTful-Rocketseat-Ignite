2189121486905210d52cec3861886484168548fd
Laroy Prado <laroyprado@gmail.com>
2023-07-25 18:39:44 -0300

    Criação de tabelas

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,17 @@
 import fastify from 'fastify'
 import { knex } from './database'
+import crypto from 'node:crypto'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const tables = await knex('sqlite_schema').select('*')
-  return tables
+  const transactions = await knex('transactions').insert({
+    id: crypto.randomUUID(),
+    title: 'Transação de teste',
+    amount: 1000,
+  })
+
+  return transactions
 })
 
 app
