8c9008b703063d8bde8c9d3edca98993053d1cf2
Laroy Prado <laroyprado@gmail.com>
2023-07-25 19:28:00 -0300

    dot Env criado e atualizações no Server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,19 @@
 import fastify from 'fastify'
 import { knex } from './database'
 import crypto from 'node:crypto'
+import { env } from './env'
 
 const app = fastify()
 
 app.get('/hello', async () => {
-  const transactions = await knex('transactions').insert({
-    id: crypto.randomUUID(),
-    title: 'Transação de teste',
-    amount: 1000,
-  })
+  const transactions = await knex('transactions').select('*')
 
   return transactions
 })
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('HTTP Server is running!')
